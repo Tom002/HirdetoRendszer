d4753de89558a8269f26298cdf5bcc0d5d06ac00
tamas.princz@edu.bme.hu <tamas.princz@edu.bme.hu>
2021-11-29 16:06:59 +0100

    Hiányzó seed service hívás

--- HirdetoRendszer/Program.cs
+++ HirdetoRendszer/Program.cs
@@ -45,6 +45,7 @@ namespace HirdetoRendszer
                     await seedService.SeedJarmuvek();
                     await seedService.SeedHirdetesKepek();
                     await seedService.SeedHirdetesek();
+                    await seedService.SeedHirdetesHelyettesitok();
                     await seedService.SeedJaratok();
                 }
 
